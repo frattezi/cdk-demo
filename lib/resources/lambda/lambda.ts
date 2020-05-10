e685d7ced79fbd01c8f47a28814888c45ca34c6d
frattezi <pfrattezi@gmail.com>
2020-05-10 20:30:56 -0300

    FIX: lambda code

--- lib/resources/lambda/lambda.ts
+++ lib/resources/lambda/lambda.ts
@@ -1,18 +1,30 @@
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { PolicyStatement } from "@aws-cdk/aws-iam"
 import * as cdk from '@aws-cdk/core';
 import * as sqs from '@aws-cdk/aws-sqs';
+import * as sns from '@aws-cdk/aws-sns';
+
 import { DynamoEventSource, SqsDlq } from '@aws-cdk/aws-lambda-event-sources';
 import * as path from 'path'
 
 export const setupNotificationLambda = (app: cdk.Construct, table: dynamodb.Table) => {
+  const notificationTopic = new sns.Topic(app, 'SNSLambda');
+  const deadLetterQueue = new sqs.Queue(app, 'deadLetterQueue');
+
+  const lambdaPolicy = new PolicyStatement()
+  lambdaPolicy.addActions("sns:Publish")
+  lambdaPolicy.addResources(notificationTopic.topicArn)
+
   const notificationFunction = new lambda.Function(app, 'Lambda', {
     code: lambda.Code.fromAsset(path.join(__dirname, 'notificationLambda')),
     handler: 'lambda_function.lambda_handler',
     runtime: lambda.Runtime.PYTHON_3_6,
+    initialPolicy: [lambdaPolicy],
+    deadLetterQueue,
   });
 
-  const deadLetterQueue = new sqs.Queue(app, 'deadLetterQueue');
+  notificationFunction.addEnvironment('TOPIC_ARN', notificationTopic.topicArn)
 
   notificationFunction.addEventSource(new DynamoEventSource(table, {
     startingPosition: lambda.StartingPosition.TRIM_HORIZON,
--- lib/resources/lambda/notificationLambda/lambda_function.py
+++ lib/resources/lambda/notificationLambda/lambda_function.py
@@ -1,14 +1,19 @@
 import json
 import boto3
+import os
 def lambda_handler(event, context):
     print(event)
     print(context)
     client = boto3.client('sns')
-    response = client.publish(
-        TopicArn='string',
-        Message='string',
-        MessageStructure='string',
-)
+    for record in event.get('Records'):
+        if record.get('eventName') == 'INSERT':
+            print(record)
+            response = client.publish(
+                TopicArn=os.getenv('TOPIC_ARN'),
+                Message='New Account created',
+                Subject='New User'
+            )
+            print(response)
 
 
 
